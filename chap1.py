8016c8b689e28e863b86fb3d196ba2a7d2931694
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-05-18 14:54:28 -0400

    P30

--- chap1.py
+++ chap1.py
@@ -1,27 +1,37 @@
 #P29
-import copy
+# import copy
 
-def recFind(inputL):
-    possL = list()
-    for i in range(len(inputL)):
-        currentI = [inputL[i]]
-        remainder = inputL[:i] + inputL[i+1:]
+# def recFind(inputL):
+#     def find(inputL):
+#         possL = list()
+#         for i in range(len(inputL)):
+#             currentI = [inputL[i]]
+#             remainder = inputL[:i] + inputL[i+1:]
 
-        if len(remainder) == 0:
-            return currentI
-        else:
-            returnL = copy.deepcopy(recFind(remainder))
+#             if len(remainder) == 0:
+#                 return currentI
+#             else:
+#                 returnL = copy.deepcopy(recFind(remainder))
 
-            for j in returnL:
-                tempCurrentI = copy.deepcopy(currentI)
+#                 for j in returnL:
+#                     tempCurrentI = copy.deepcopy(currentI)
 
-                if isinstance(j, list):
-                    for k in j:
-                        tempCurrentI.append(k)
-                    possL.append(tempCurrentI)
-                else:
-                    tempCurrentI.append(j)
-                    possL.append(tempCurrentI)
-    return possL
+#                     if isinstance(j, list):
+#                         for k in j:
+#                             tempCurrentI.append(k)
+#                         possL.append(tempCurrentI)
+#                     else:
+#                         tempCurrentI.append(j)
+#                         possL.append(tempCurrentI)
+#         return possL
+#     return [''.join(b) for b in find(inputL)]
 
-a = [''.join(b) for b in recFind(['a','c','t'])]
+#P30
+# def counter(number, count=0):
+#     if number < 2:
+#         raise ValueError('too small')
+#     else:
+#         while number >= 2:
+#             number /= 2
+#             count += 1
+#     return count
