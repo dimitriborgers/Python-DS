4c4d7cf6cf44bcb1ded1b82ce279088e942aee2c
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-05-18 12:32:20 -0400

    example 1 success

--- chap1.py
+++ chap1.py
@@ -1,19 +1,22 @@
 import copy
 
-def recFind(inputL):
+def recFind(inputL, level = 1, possL = list(), final = list()):
     for i in range(len(inputL)):
         currentI = [inputL[i]]
         remainder = inputL[:i] + inputL[i+1:]
         print("This is currenI, ", currentI)
         print("This is remainder, ", remainder)
+        print("This is the level, ", level)
 
         if len(remainder) == 0:
             return currentI
         else:
-            returnL = copy.deepcopy(recFind(remainder))
+            level += 1
+            returnL = copy.deepcopy(recFind(remainder, level, possL, final))
+            level -= 1
+            print("This is the level2, ", level)
             print("This is returnL, ",returnL)
             for j in returnL:
-                print("This is returnL2, ", returnL)
                 print("This is j, ", j)
                 tempCurrentI = copy.deepcopy(currentI)
                 print("This is tempCurrentBefore, ", tempCurrentI)
@@ -21,7 +24,6 @@ def recFind(inputL):
                     for k in j:
                         tempCurrentI.append(k)
                         print("This is tempCurrentAfter, ", tempCurrentI)
-                    # tempCurrentI = copy.deepcopy(currentI)
                     possL.append(tempCurrentI)
                 else:
                     tempCurrentI.append(j)
@@ -29,14 +31,16 @@ def recFind(inputL):
                     possL.append(tempCurrentI)
                 print("This is possL, ", possL)
             print("reaching this point?")
+        print("reaching this point?2")
+        if level == 1:
+            for a in possL:
+                if len(a) == len(inputL):
+                    final.append(a)
+            possL = list()
+            print("this is final", final)
+    print("reaching this point?3")
+    level -= 1
     return possL
 
-possL = list()
-final = list()
 print("start")
-a = recFind([1,2,3,4])
-for l in a:
-    if len(l) == 3 and len(set(l)) == len(l):
-        final.append(l)
-print(len(final))
-
+print(recFind([1,2,3,4]))
