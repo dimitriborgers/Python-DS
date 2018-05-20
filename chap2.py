b6ea0a8b32d302b68dd67a2d0072cd122311af90
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-05-20 15:01:40 -0400

    R4-R6

--- chap2.py
+++ chap2.py
@@ -1,7 +1,48 @@
 # R1
 # None coding
+
 # R2
 # None coding
+
 # R3
 # None coding
+
 # R4
+# class Flower:
+#     def __init__(self, name, petals, price):
+#         self.name = name
+#         self.petals = petals
+#         self.price = price
+
+#     """return price of flower"""
+#     def getPrice(self):
+#         return self.price
+
+#     """set price of flower"""
+#     def setPrice(self, price):
+#         self.price = price
+#         return self.price
+
+# if __name__ == '__main__':
+#     flower1 = Flower("rose", 10, 3.99)
+
+# R5
+# def charge(self, price):
+#     try:
+#         if price + self._balance > self._limit:
+#             return False
+#         else:
+#             self._balance += price
+#             return True
+#     except TypeError:
+#         print('Must be a number')
+#         raise
+
+# def make_payment(self, amount):
+#     try:
+#         self._balance -= amount
+#     except TypeError:
+#         print('Must be number')
+
+R6
+
