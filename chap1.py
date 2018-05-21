af44f0b7e7864a172ab2dbf394411a59c9a94454
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-05-21 11:51:45 -0400

    R18

--- chap1.py
+++ chap1.py
@@ -1,147 +1,147 @@
 #P29
-# import copy
+import copy
 
-# def recFind(inputL):
-#     def find(inputL):
-#         possL = list()
-#         for i in range(len(inputL)):
-#             currentI = [inputL[i]]
-#             remainder = inputL[:i] + inputL[i+1:]
+def recFind(inputL):
+    def find(inputL):
+        possL = list()
+        for i in range(len(inputL)):
+            currentI = [inputL[i]]
+            remainder = inputL[:i] + inputL[i+1:]
 
-#             if len(remainder) == 0:
-#                 return currentI
-#             else:
-#                 returnL = copy.deepcopy(recFind(remainder))
+            if len(remainder) == 0:
+                return currentI
+            else:
+                returnL = copy.deepcopy(recFind(remainder))
 
-#                 for j in returnL:
-#                     tempCurrentI = copy.deepcopy(currentI)
+                for j in returnL:
+                    tempCurrentI = copy.deepcopy(currentI)
 
-#                     if isinstance(j, list):
-#                         for k in j:
-#                             tempCurrentI.append(k)
-#                         possL.append(tempCurrentI)
-#                     else:
-#                         tempCurrentI.append(j)
-#                         possL.append(tempCurrentI)
-#         return possL
-#     return [''.join(b) for b in find(inputL)]
+                    if isinstance(j, list):
+                        for k in j:
+                            tempCurrentI.append(k)
+                        possL.append(tempCurrentI)
+                    else:
+                        tempCurrentI.append(j)
+                        possL.append(tempCurrentI)
+        return possL
+    return [''.join(b) for b in find(inputL)]
 
 #P30
-# def counter(number, count=0):
-#     if number < 2:
-#         raise ValueError('too small')
-#     else:
-#         while number >= 2:
-#             number /= 2
-#             count += 1
-#     return count
+def counter(number, count=0):
+    if number < 2:
+        raise ValueError('too small')
+    else:
+        while number >= 2:
+            number /= 2
+            count += 1
+    return count
 
 # P31
-# def makechange(charge, given):
-#     difference = given - charge
-#     hund = difference // 100
-#     difference -= hund * 100
-#     twenties = difference // 20
-#     difference -= twenties * 20
-#     return ('hundred bills {0} twenty bills {1}'.format(hund, twenties))
+def makechange(charge, given):
+    difference = given - charge
+    hund = difference // 100
+    difference -= hund * 100
+    twenties = difference // 20
+    difference -= twenties * 20
+    return ('hundred bills {0} twenty bills {1}'.format(hund, twenties))
 
 # P32
-# def calculator():
-#     allL = list()
-#     temL = list()
-#     finished = False
-#     while finished == False:
-#         inputted = input('enter number, operator, or finish ')
-#         if inputted == 'finish':
-#             finished = True
-#         else:
-#             allL.append(inputted)
-#             print(allL)
-#     for i in range(len(allL)):
-#         if allL[i] == '*':
-#             times = int(allL[i-1])*int(allL[i+1])
-#             temL.pop()
-#             temL.append(times)
-#         elif allL[i-1] == '*':
-#             continue
-#         elif allL[i] == '/':
-#             divide = int(allL[i-1])/int(allL[i+1])
-#             temL.pop()
-#             temL.append(divide)
-#         elif allL[i-1] == '/':
-#             continue
-#         else:
-#             temL.append(allL[i])
-#     allL = temL
-#     temL = list()
-#     for i in range(len(allL)):
-#         if allL[i] == '+':
-#             times = int(allL[i-1])+int(allL[i+1])
-#             temL.pop()
-#             temL.append(times)
-#         elif allL[i-1] == '+':
-#             continue
-#         elif allL[i] == '-':
-#             divide = int(allL[i-1]) - int(allL[i+1])
-#             temL.pop()
-#             temL.append(divide)
-#         elif allL[i-1] == '-':
-#             continue
-#         else:
-#             temL.append(allL[i])
-#     return allL
+def calculator():
+    allL = list()
+    temL = list()
+    finished = False
+    while finished == False:
+        inputted = input('enter number, operator, or finish ')
+        if inputted == 'finish':
+            finished = True
+        else:
+            allL.append(inputted)
+            print(allL)
+    for i in range(len(allL)):
+        if allL[i] == '*':
+            times = int(allL[i-1])*int(allL[i+1])
+            temL.pop()
+            temL.append(times)
+        elif allL[i-1] == '*':
+            continue
+        elif allL[i] == '/':
+            divide = int(allL[i-1])/int(allL[i+1])
+            temL.pop()
+            temL.append(divide)
+        elif allL[i-1] == '/':
+            continue
+        else:
+            temL.append(allL[i])
+    allL = temL
+    temL = list()
+    for i in range(len(allL)):
+        if allL[i] == '+':
+            times = int(allL[i-1])+int(allL[i+1])
+            temL.pop()
+            temL.append(times)
+        elif allL[i-1] == '+':
+            continue
+        elif allL[i] == '-':
+            divide = int(allL[i-1]) - int(allL[i+1])
+            temL.pop()
+            temL.append(divide)
+        elif allL[i-1] == '-':
+            continue
+        else:
+            temL.append(allL[i])
+    return allL
 
 # P33
-# def calc(previous=None):
-#     finished = False
-#     list1 = []
-#     if previous != None:
-#         list1.append(previous)
-#     while len(list1) < 3 and finished == False:
-#         inputted = input('#, op, finish or clear ')
-#         if inputted == 'clear':
-#             calc()
-#         elif inputted == 'finish':
-#             finished = True
-#         else:
-#             list1.append(inputted)
-#     if finished == False and isinstance(int(list1[0]), int) and isinstance(int(list1[2]), int) and list1[1] == '*':
-#         temp = int(list1[0])*int(list1[2])
-#         print(temp)
-#         calc(temp)
+def calc(previous=None):
+    finished = False
+    list1 = []
+    if previous != None:
+        list1.append(previous)
+    while len(list1) < 3 and finished == False:
+        inputted = input('#, op, finish or clear ')
+        if inputted == 'clear':
+            calc()
+        elif inputted == 'finish':
+            finished = True
+        else:
+            list1.append(inputted)
+    if finished == False and isinstance(int(list1[0]), int) and isinstance(int(list1[2]), int) and list1[1] == '*':
+        temp = int(list1[0])*int(list1[2])
+        print(temp)
+        calc(temp)
 
 # P34
-# from random import *
+from random import *
 
-# list2 = ['$','#','FLor','??']
+list2 = ['$','#','FLor','??']
 
-# for i in range(100):
-#     list1 = list('I will never spam my friends again')
-#     randError = randrange(len(list1))
-#     list1[randError] = choice(list2)
-#     print(i+1,''.join(list1))
+for i in range(100):
+    list1 = list('I will never spam my friends again')
+    randError = randrange(len(list1))
+    list1[randError] = choice(list2)
+    print(i+1,''.join(list1))
 
 # P35
-# from random import *
+from random import *
 
-# def birthparadox(list1):
-#     list2 = []
-#     list3 = []
-#     for i in list1:
-#         for j in range(i):
-#             list2.append(randrange(365))
-#         if len(set(list2)) != len(list2):
-#             list3.append('paradox')
-#     return len(list3)/len(list1)
-# print(birthparadox([5,10,15,20,15,30, 35, 40]))
+def birthparadox(list1):
+    list2 = []
+    list3 = []
+    for i in list1:
+        for j in range(i):
+            list2.append(randrange(365))
+        if len(set(list2)) != len(list2):
+            list3.append('paradox')
+    return len(list3)/len(list1)
+print(birthparadox([5,10,15,20,15,30, 35, 40]))
 
 #P36
-# def counter():
-#     dic = {}
-#     for i in range(10):
-#         inputted = input('input word ')
-#         if inputted not in dic:
-#             dic[inputted] = 1
-#         else:
-#             dic[inputted] += 1
-#     return dic
+def counter():
+    dic = {}
+    for i in range(10):
+        inputted = input('input word ')
+        if inputted not in dic:
+            dic[inputted] = 1
+        else:
+            dic[inputted] += 1
+    return dic
--- chap2.py
+++ chap2.py
@@ -1,97 +1,168 @@
 # R1
-# Non-coding
+Non-coding
 
 # R2
-# Non-coding
+Non-coding
 
 # R3
-# Non-coding
+Non-coding
 
 # R4
-# class Flower:
-#     def __init__(self, name, petals, price):
-#         self.name = name
-#         self.petals = petals
-#         self.price = price
+class Flower:
+    def __init__(self, name, petals, price):
+        self.name = name
+        self.petals = petals
+        self.price = price
 
-#     """return price of flower"""
-#     def getPrice(self):
-#         return self.price
+    """return price of flower"""
+    def getPrice(self):
+        return self.price
 
-#     """set price of flower"""
-#     def setPrice(self, price):
-#         self.price = price
-#         return self.price
+    """set price of flower"""
+    def setPrice(self, price):
+        self.price = price
+        return self.price
 
-# if __name__ == '__main__':
-#     flower1 = Flower("rose", 10, 3.99)
+if __name__ == '__main__':
+    flower1 = Flower("rose", 10, 3.99)
 
 # R5
-# def charge(self, price):
-#     try:
-#         if price + self._balance > self._limit:
-#             return False
-#         else:
-#             self._balance += price
-#             return True
-#     except TypeError:
-#         print('Must be a number')
-#         raise
+def charge(self, price):
+    try:
+        if price + self._balance > self._limit:
+            return False
+        else:
+            self._balance += price
+            return True
+    except TypeError:
+        print('Must be a number')
+        raise
 
-# def make_payment(self, amount):
-#     try:
-#         self._balance -= amount
-#     except TypeError:
-#         print('Must be number')
+def make_payment(self, amount):
+    try:
+        self._balance -= amount
+    except TypeError:
+        print('Must be number')
 
 # R6
-# def make_payment(self, amount):
-#     try:
-#         if amount < 0:
-#             raise ValueError('Must be positive')
-#         self._balance -= amount
-#     except TypeError
-#         print('Must be number')
+def make_payment(self, amount):
+    try:
+        if amount < 0:
+            raise ValueError('Must be positive')
+        self._balance -= amount
+    except TypeError
+        print('Must be number')
 
 # R7
-# def __init__(self, customer, bank, acnt, limit, balance=0)
+def __init__(self, customer, bank, acnt, limit, balance=0)
 
 # R8
-# Non-coding
+Non-coding
 
 # R9
-# def __sub__(self, other):
-#     if len(self) != len(other):
-#         raise ValueError('Dimensions must agree')
-#     result = Vector(len(self))
-#     for j in range(len(self)):
-#         resut[j] = self[j] - other[j]
-#     return result
+def __sub__(self, other):
+    if len(self) != len(other):
+        raise ValueError('Dimensions must agree')
+    result = Vector(len(self))
+    for j in range(len(self)):
+        resut[j] = self[j] - other[j]
+    return result
 
 # R10
-# def __neg__(self):
-#     result = Vector(len(self))
-#     for j in range(len(self)):
-#         result[j] = -self[j]
-#     return result
+def __neg__(self):
+    result = Vector(len(self))
+    for j in range(len(self)):
+        result[j] = -self[j]
+    return result
 
 # R11
-# Relies on __len__ member function, which only works with vectors.
-# def __len__(self):
-#     if isinstance(self, list):
-#         return len(self)
-#     else:
-#         return len(self._coords)
+def __radd__(self, other):
+    if len(self) != len(other):
+        raise ValueError('dimensions must agree')
+    result = Vector(len(self))
+    for j in range(len(self)):
+        result[j] = self[j] + other[j]
+    return result
 
 # R12
-# def __mul__(self, number):
-#     if not isinstance(number, int):
-#         raise TypeError("must be a number")
-#     else:
-#         result = Vector(len(self))
-#         for j in range(len(self)):
-#             result[j] = self[j] * number
-#     return result
+def __mul__(self, number):
+    if not isinstance(number, int):
+        raise TypeError("must be a number")
+    else:
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] * number
+    return result
 
 # R13
+def __rmul__(self, number):
+    if not isinstance(number, int):
+        raise TypeError("must be a number")
+    else:
+        result = Vector(len(self))
+        for j in range(len(self)):
+            result[j] = self[j] * number
+    return result
+
+# R14
+def __mul__(self, other):
+    if len(self) != len(other):
+        raise ValueError('This does not work')
+    else:
+        result = 0
+        for i in range(len(self))
+        result += self[i]*other[i]
+    return result
+
+# R15
+def __init__(self, d):
+    if isinstance(d, int):
+        self._coords = [0]*d
+    else:
+        self._coords = [0]*len(d)
+        for i in range(len(self._coords)):
+            self._coords[i] = d[i]
+
+# R16
+Non-coding
+
+# R17
+Non-coding
+"""create class for example1 instead"""
+class Mammal():
+    def __init__(self,gender,age):
+        self.gender = gender
+        self.age = age
+
+    def growOlder(self, number):
+        self.age += number
+
+    def getGender(self):
+        return self.gender
+
+class Goat(Mammal):
+    def __init__(self,gender,age,farm):
+        super().__init__(gender, age)
+        self.farm = farm
+
+    def growOlder(self, number):
+        super().growOlder(number)
+
+    def getAge(self):
+        return self.age
+
+# R18
+
+
+# R19
+Non-coding
+
+# R20
+Non-coding
+
+# R21
+Non-coding
+
+# R22
+
 
