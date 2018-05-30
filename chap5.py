073e1d3fe41b00c8de8519bd9a80382c8f48baf7
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-05-30 14:50:43 -0400

    C30

--- chap5.py
+++ chap5.py
@@ -141,34 +141,148 @@ Non-Coding
 Non-Coding
 
 # C21
+from time import time
 
+letters = ''
+with open('test.txt','r') as f:
+    document = f.read()
+start = time()
+for c in document:
+    if c.isalpha():
+        letters += c
+end = time()
+print(end-start)
+
+temp = []
+start = time()
+for c in document:
+    if c.isalpha():
+        temp.append(c)
+    letters = ''.join(temp)
+end = time()
+print(start - end)
 
 # C22
+from time import time
 
+data = [i for i in range(10000000)]
+target = ['a','b']
+target2 = ['a','b']
+start = time()
+for i in data:
+    target.append(i)
+end = time()
+print(start-end)
+
+start = time()
+target2.extend(data)
+end = time()
+print(start-end)
 
 # C23
+from time import time
 
+start = time()
+data = [i for i in range(10000000)]
+end = time()
+print(start-end)
+
+start = time()
+target=[]
+for i in range(10000000):
+    target.append(i)
+end = time()
+print(start-end)
 
 # C24
+from time import time
 
+data = [i for i in range(10000)]
+
+start = time()
+for i in range(int(len(data)/2)):
+    data.remove(i)
+end = time()
+print(start-end)
+
+data = [i for i in range(10000)]
+
+start = time()
+for i in range(int(len(data)/2)):
+    data.remove(data[len(data)//2])
+end = time()
+print(start-end)
+
+data = [i for i in range(10000)]
+
+start = time()
+for i in range(int(len(data)/2)):
+    data.remove(data[len(data)-1])
+end = time()
+print(start-end)
 
 # C25
-
+def remove_all(data, value):
+    original_length = len(data)
+    for i in range(len(data)):
+        if i > original_length-1:
+            break
+        while data[i] == value:
+            data.remove(data[i])
+            original_length -= 1
+        print(data[i])
 
 # C26
-
+def find(seq):
+    seq.sort()
+    for i in range(len(seq)):
+        if seq[i] == seq[i+1] == seq[i+2] == seq[i+3] == seq[i+4]:
+            print(seq[i])
+            break
 
 # C27
-
+Non-Coding
 
 # C28
 Non-Coding
 
 # C29
-
+def find(A,B,outcome=[]):
+    for x in A:
+        for y in B:
+            if x[1] == y[0]:
+                outcome.append([x[0],x[1],y[1]])
+    return outcome
 
 # C30
+"""Running time: O(nlogn)"""
+def mergesort(S):
+    half = len(S)//2
+    left = S[:half]
+    right = S[half:]
 
+    if half == 0:
+        return right
+    else:
+        tempL = mergesort(left)
+        tempR = mergesort(right)
+        outcome = []
+        currentL = 0
+        currentR = 0
+        while len(outcome) != len(tempL) + len(tempR):
+            if currentL == len(tempL):
+                outcome.append(tempR[currentR])
+                currentR += 1
+            elif currentR == len(tempR):
+                outcome.append(tempL[currentL])
+                currentL += 1
+            elif tempL[currentL] <= tempR[currentR]:
+                outcome.append(tempL[currentL])
+                currentL += 1
+            else:
+                outcome.append(tempR[currentR])
+                currentR += 1
+        return outcome
 
 # C31
 
